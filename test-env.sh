80def798cf4db8b87f58764fbc310d60b99c2e30
Kent Behrends <kent@bci.com>
2025-10-13 17:56:52 -0700

    Test scripts moved to tests/

--- test-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Test that .env is loaded
-export HISTORY_POINTS=12345
-./tempest-homekit-go --use-generated-weather --disable-homekit --read-history 2>&1 | grep -i "generating.*historical" | head -1
